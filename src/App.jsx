2e0af25482d8b1143489073e11a49d0f35e0d87d
shadycodee <mikojohn158@gmail.com>
2024-12-15 21:44:25 +0800

    `Updated App.css and App.jsx with new styles and components`

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Paper from '@mui/material/Paper'
 import './App.css';
 function App() {
   return (
@@ -6,8 +7,15 @@ function App() {
     <div className='header'> 
       <h1>JOHN OSCAR ROBLE </h1>
       <h3>Software Engineer</h3>
+      
 
     </div>
+    <Paper elevation={3} 
+    sx={{backgroundColor: '#4d82d6', 
+        height: '100vh',
+        marginLeft: '10%',
+        marginRight: '10%'}}></Paper>
+
    
     </>
 
