b9525609486c9abcf9a2d64b8cf9181b9f966167
shadycodee <mikojohn158@gmail.com>
2024-12-15 22:20:30 +0800

    `Refactored CSS styles and added new HTML elements to App component`

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Paper from '@mui/material/Paper'
 import './App.css';
+import { colors } from '@mui/material';
 function App() {
   return (
     <>
@@ -14,7 +15,15 @@ function App() {
     sx={{backgroundColor: '#4d82d6', 
         height: '100vh',
         marginLeft: '10%',
-        marginRight: '10%'}}></Paper>
+        marginRight: '10%'}}>
+          <div className='about-me-section'>
+            <h4 style={{margin: '10px'}}>Content</h4>
+            
+
+          </div>
+          <div style={{display: 'flex', justifyContent: 'center', alignContent: 'center', alignItems:'center', marginTop: '20%'}}><h3 style={{color: 'white'}}>Under construction...</h3></div>
+         
+        </Paper>
 
    
     </>
