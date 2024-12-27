d0219b134b96b723190bd64672805c96cd51a505
shadycodee <mikojohn158@gmail.com>
2024-12-27 12:48:30 +0800

    Update About component with personal introduction and interests

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div>Hello! I’m currently a 4th-year BSIT student with a passion for building and creating. My love for technology has driven me to explore programming, with Python and SQL being my favorite tools for solving problems and developing projects. In my free time, I enjoy dabbling in pixel art, where I’m still practicing and honing my skills—it’s a fun and creative outlet that challenges me in a different way. I’m always eager to learn, grow, and embrace new challenges as I continue my journey in the tech world.</div>
   )
 }
 
